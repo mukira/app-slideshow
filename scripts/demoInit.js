8d6db4b0f281ccba094f5bc765d930d02c989e5d
maxwellito <duluc.maxime@gmail.com>
2015-04-28 01:16:17 +0100

    Fix mobile popup warning position

--- scripts/demoInit.js
+++ scripts/demoInit.js
@@ -19,7 +19,7 @@ ga('send', 'pageview');
 // Mobile test
 if (/(android|blackberry|iphone|ipod)/i.test(navigator.userAgent) && window.innerWidth < 500) {
   var warningDom = document.createElement('div');
-  warningDom.setAttribute('style', 'position: fixed; top: 0; left: 0; width: 100%; height: 100%; padding: 1rem; font-family: \'Helvetica Neue\', Helvetica, sans-serif; font-weight: 100; background: #181F21; color: #86E0C4; box-sizing: border-box; z-index: 99999;');
+  warningDom.setAttribute('style', 'position: fixed; top: 0; left: 0; bottom: 0; right: 0; padding: 1rem; font-family: \'Helvetica Neue\', Helvetica, sans-serif; font-weight: 100; background: #181F21; color: #86E0C4; box-sizing: border-box; z-index: 99999;');
   warningDom.innerHTML =  '<p style="font-size: 1.6rem;">Sorry,<br>you must visit this page on a desktop or a larger screen device.<br>But keep your smartphone, you will need it :-)</p>' +
                           '<p style="font-size: 1rem; font-style: italic;">If you think this message has been displayed by mistake, click on the top right corner cross.</p>' +
                           '<span onclick="this.parentNode.remove();ga(\'send\',\'event\',\'mobile_popup\',\'close\');" style="position:absolute; top: 0; right: 0; padding: 1rem; line-height: 0.5;">&#215;</span>';
